d6d74afd6f75c65e1b0478db363cdfcd1e6bd552
Johan Karlsson <DonKarlssonSan@gmail.com>
2017-11-26 21:35:14 +0100

    Initial commit, just says "Hello!" every 30 mins.

---
+++ flowfieldimagetweetbot/index.js
@@ -0,0 +1,20 @@
+const Twit = require('twit')
+const fs = require('fs');
+
+module.exports = function (context, myTimer) {
+    
+    let twit = new Twit({
+      consumer_key: process.env.CONSUMER_KEY,
+      consumer_secret: process.env.CONSUMER_SECRET,
+      access_token: process.env.ACCESS_TOKEN,
+      access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+      timeout_ms: 60*1000,
+    });
+    twit.post('statuses/update', { status: 'Hello!' }, function(err, data, response) {
+        context.log(data);
+        context.log.error(err);
+        //console.log(err, data, response);
+        context.done();
+    });
+};
+
