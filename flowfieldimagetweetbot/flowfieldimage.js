ac79a7863cbf9af8e15d6afa766f25c6fb6c2cb9
Johan Karlsson <DonKarlssonSan@gmail.com>
2017-11-27 21:29:42 +0100

    Add noise to the generated image Add simplex-noise module
    Add readme

--- flowfieldimagetweetbot/flowfieldimage.js
+++ flowfieldimagetweetbot/flowfieldimage.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const Jimp = require('jimp');
-
+const SimplexNoise = require('simplex-noise');
 
 module.exports.generate = function () {
+    let simplex = new SimplexNoise(Math.random);
     let width = 500;
     let height = 500;
-    let image = new Jimp(width, height, 0x990099FF);
+    let image = new Jimp(width, height, 0x000000FF);
+
+    for(let x = 0; x < width; x++) {
+        for(let y = 0; y < height; y++) {
+            let n = (simplex.noise2D(x / 100, y / 100) + 1) / 2 * 255;
+            let color = Jimp.rgbaToInt(n, 0, n, 255);
+            image.setPixelColor(color, x, y);
+        }
+    }
     
     // TODO: keep the data in memory instead of writing to disk and then reading from disk
     return new Promise((resolve, reject) => {
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Flowfield Image Tweet Bot
+
+I tweet flow field images here: https://twitter.com/FlowfieldImage
+
+ * What is a flowfield?
+ * How can you make one?
+ * What can you do with it?
+
+Answers here: https://codepen.io/DonKarlssonSan/post/particles-in-simplex-noise-flow-field
